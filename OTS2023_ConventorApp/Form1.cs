96c042c1af60a72f0669085be67b22ec96fb11fc
markoisajlovic1 <markoisajlovic38@gmail.com>
2024-03-07 19:30:19 +0100

    zadatak1

--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+ 
 
 namespace OTS2023_ConventorApp
 {
@@ -16,5 +18,55 @@ namespace OTS2023_ConventorApp
         {
             InitializeComponent();
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Pounds";
+            label2.Text = "Kg";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Feet";
+            label2.Text = "Meters";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                float weight = float.Parse(textBox1.Text);
+                convertToKg(weight);
+            }
+
+            if (radioButton2.Checked)
+            {
+                float height = float.Parse(textBox2.Text);
+                convertToM(height);
+            }
+
+        }
+
+
+        //zadatak
+        public void convertToKg(float weight)
+        {
+            double kg = weight * 0.4;
+            textBox2.Text = kg.ToString();
+        }
+
+        public void convertToM(float height)
+        {
+            double m = height * 0.3;
+            textBox2.Text = m.ToString();
+        }
+
+
     }
 }
